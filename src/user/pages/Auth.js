da3959385c024be0c958fcc86b044ef965d6b148
MashhoodYousafGhuman <mashhoodyousaf24@gmail.com>
2025-05-28 01:43:07 +0500

    adding place post request connecting to backend

--- src/user/pages/Auth.js
+++ src/user/pages/Auth.js
@@ -67,7 +67,7 @@ const Auth = () => {
     if (isLoginMode) {
 
       try {
-        await sendRequest('http://localhost:5000/api/users/login', 'POST',
+        const responseData = await sendRequest('http://localhost:5000/api/users/login', 'POST',
           JSON.stringify({
             email: formState.inputs.email.value,
             password: formState.inputs.password.value
@@ -77,14 +77,14 @@ const Auth = () => {
           },
         );
 
-        auth.login();
+        auth.login(responseData.user.id);
       } catch (err) {
         console.log('err', err)
       }
 
     } else {
       try {
-        await sendRequest('http://localhost:5000/api/users/signup', 'POST',
+        const responseData = await sendRequest('http://localhost:5000/api/users/signup', 'POST',
           JSON.stringify({
             name: formState.inputs.name.value,
             email: formState.inputs.email.value,
@@ -95,7 +95,7 @@ const Auth = () => {
           }
         );
 
-        auth.login();
+        auth.login(responseData.user.id);
       } catch (err) {
         console.log('err', err)
       }
