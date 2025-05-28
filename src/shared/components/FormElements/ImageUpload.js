7665bd7b1abe7180a99dcb902972464959009af5
MashhoodYousafGhuman <mashhoodyousaf24@gmail.com>
2025-05-29 01:59:59 +0500

    fileHandling...

---
+++ src/shared/components/FormElements/ImageUpload.js
@@ -0,0 +1,65 @@
+import React, { useEffect, useRef, useState } from 'react'
+
+import Button from './Button'
+import './ImageUpload.css'
+const ImageUpload = props => {
+	const [file, setFile] = useState();
+	const [previewUrl, setPreviewUrl] = useState();
+	const [isValid, setIsValid] = useState();
+
+	const filePickerRef = useRef();
+
+	useEffect(() => {
+		if (!file) {
+			return;
+		}
+		const fileReader = new FileReader();
+		fileReader.onload = () => {
+			setPreviewUrl(fileReader.result);
+		};
+		fileReader.readAsDataURL(file);
+	}, [file])
+
+	const pickedHandler = event => {
+		console.log(event.target)
+		let pickedFile;
+		let fileIsValid = isValid;
+		if (event.target.files && event.target.length === 1) {
+			pickedFile = event.target.files[0];
+			setFile(pickedFile);
+			setIsValid(true);
+			fileIsValid = true;
+		} else {
+			setIsValid(false);
+			fileIsValid = false;
+		}
+		props.onInput(props.id, pickedFile, fileIsValid);
+	};
+
+	const pickImageHandler = () => {
+		filePickerRef.current.click();
+	};
+
+	return (
+		<div className='form-control'>
+			<input
+				id={props.id}
+				style={{ display: 'none' }}
+				type="file"
+				ref={filePickerRef}
+				onChange={pickedHandler}
+				accept='.jpeg, .png, .jpg'
+			/>
+			<div className={`image-upload ${props.center && 'center'}`}>
+				<div className="image-upload__preview">
+					{previewUrl && <img src={previewUrl} alt="preview" />}
+					{!previewUrl && <p>Please select an image</p>}
+				</div>
+				<Button type='button' onClick={pickImageHandler}>Select Image</Button>
+			</div>
+			{!isValid && <p>{props.errorText}</p>}
+		</div>
+	)
+}
+
+export default ImageUpload;
--- src/user/pages/Auth.js
+++ src/user/pages/Auth.js
@@ -5,6 +5,7 @@ import Input from '../../shared/components/FormElements/Input';
 import Button from '../../shared/components/FormElements/Button';
 import ErrorModal from '../../shared/components/UIElements/ErrorModal';
 import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner';
+import ImageUpload from '../../shared/components/FormElements/ImageUpload';
 import {
   VALIDATOR_EMAIL,
   VALIDATOR_MINLENGTH,
@@ -42,7 +43,8 @@ const Auth = () => {
       setFormData(
         {
           ...formState.inputs,
-          name: undefined
+          name: undefined,
+          image: undefined
         },
         formState.inputs.email.isValid && formState.inputs.password.isValid
       );
@@ -53,6 +55,10 @@ const Auth = () => {
           name: {
             value: '',
             isValid: false
+          },
+          image: {
+            value: null,
+            isValid:false
           }
         },
         false
@@ -63,6 +69,8 @@ const Auth = () => {
 
   const authSubmitHandler = async event => {
     event.preventDefault();
+
+    console.log(formState.inputs)
 
     if (isLoginMode) {
 
@@ -122,6 +130,7 @@ const Auth = () => {
               onInput={inputHandler}
             />
           )}
+          {!isLoginMode && <ImageUpload center id='image' onInput={inputHandler} />}
           <Input
             element="input"
             id="email"
